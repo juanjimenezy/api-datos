a931cd40196733272a1471ca339f83d8b6c8fe78
Juan Jimenez <juanjiimenez8@gmail.com>
2023-04-25 14:26:19 -0500

    estable

--- index.js
+++ index.js
@@ -7,39 +7,43 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static('public'))
 
 app.post("/form", (req, res) => {
-    console.log(req.body);
     const{id,nombre,apellido, Titulo, Autor, Editorial, Año}=req.body
     if (!nombre || !apellido) return res.redirect('/error.html');
     const path = `${__dirname}/public/data/id_${id}.txt`;
-    //res.send(`Datos enviados con exito! 
-    //            <a href="/">Volver</a>
-    //            <a href="localhost:${server_port}/download/${id}">descargar</a>`);
-    fs.writeFileSync(path,`${id}, ${nombre}, ${apellido}, ${Titulo}, ${Autor}, ${Editorial}, ${Año}`,(error)=>{
+    const data = `${id}, ${nombre}, ${apellido}, ${Titulo}, ${Autor}, ${Editorial}, ${Año}`;
+    escribirArchivo(path,data);
+    descargarArchivo(res,path);
+    success(res,id);
+});
+
+
+escribirArchivo = async (path, data) => {
+   await fs.writeFileSync(path,data,(error)=>{
         if (error) {
             console.log(`Error: ${error}`)
         }
     });
+};
 
-    res.download(path);
+descargarArchivo = async (res,path) => {
+    await res.download(path);
+}
+
+success = async (res,id) => {
+    await res.send(`Datos enviados con exito! 
+    <div>
+    <a href="/">Volver</a>
+    </div>
+    <a href="localhost:${server_port}/download/${id}">descargar</a>`);
+}
 
 
 
-    res.send(`Datos enviados con exito! 
-                <div>
-                <a href="/">Volver</a>
-                </div>
-                <a href="localhost:${server_port}/download/${id}">descargar</a>`);
-    
-});
-
-
- app.get('/download/:id', (req,res) => {
+app.get('/download/:id', (req,res) => {
     const id = parseInt(req.params.id);
     const path = `${__dirname}/public/data/id_${id}.txt`;
-    res.download(path);
+    descargarArchivo(res,path);
 });
-
-
 
 
 app.listen(server_port, () => {console.log("Server Start! ");});
