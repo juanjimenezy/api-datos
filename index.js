68ee2baff6a407c7d567e6a54b3dd52630dc30cc
Juan Jimenez <juanjiimenez8@gmail.com>
2023-04-25 13:57:35 -0500

    estabñe

--- index.js
+++ index.js
@@ -9,14 +9,34 @@ app.use(express.static('public'))
 app.post("/form", (req, res) => {
     console.log(req.body);
     const{id,nombre,apellido, Titulo, Autor, Editorial, Año}=req.body
-    if (!nombre || !apellido) return res.redirect('/error.html')
-    res.send(`Datos enviados con exito! <a href="/">Volver</a>`);
-    fs.writeFile(`./public/data/id_${id}.txt`,`${id}, ${nombre}, ${apellido}, ${Titulo}, ${Autor}, ${Editorial}, ${Año}`,(error)=>{
+    if (!nombre || !apellido) return res.redirect('/error.html');
+    const path = `${__dirname}/public/data/id_${id}.txt`;
+    //res.send(`Datos enviados con exito! 
+    //            <a href="/">Volver</a>
+    //            <a href="localhost:${server_port}/download/${id}">descargar</a>`);
+    fs.writeFileSync(path,`${id}, ${nombre}, ${apellido}, ${Titulo}, ${Autor}, ${Editorial}, ${Año}`,(error)=>{
         if (error) {
             console.log(`Error: ${error}`)
         }
-    } )
+    });
 
+    res.download(path);
+
+
+
+    res.send(`Datos enviados con exito! 
+                <div>
+                <a href="/">Volver</a>
+                </div>
+                <a href="localhost:${server_port}/download/${id}">descargar</a>`);
+    
+});
+
+
+ app.get('/download/:id', (req,res) => {
+    const id = parseInt(req.params.id);
+    const path = `${__dirname}/public/data/id_${id}.txt`;
+    res.download(path);
 });
 
 
